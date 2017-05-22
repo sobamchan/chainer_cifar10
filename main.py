e140901456c7042cae672b5c788ff0f7764ecf49
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2017-05-22 12:36:41 +0900

    pass optimizer from main to trainer

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+from chainer import optimizers
+
 from sobamchan.sobamchan_cifar10 import train
 from model import CNN, ResCNN, ResNCNN, ExpoCNN, SigCNN, AutoResNCNN
 from resnet import ResNet
 
 def main():
-    train(AutoResNCNN)
+    opts = {}
+
+    optimizer = optimizers.AdaGrad()
+    opts['optimizer'] = optimizer
+    opts['model'] = CNN
+
+    train(opts)
 
 if __name__ == '__main__':
     main()
